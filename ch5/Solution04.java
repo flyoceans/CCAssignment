dd60971b10d55eb867c7480f8bea64ba0fc2352b
flyoceans <Peng@yangqiudeMBP.wv.cc.cmu.edu>
2015-10-05 20:35:32 -0400

    update

--- ch5/Solution04.java
+++ ch5/Solution04.java
@@ -1,4 +1,45 @@
-
+/*
+ * This algorithm was revised after reviewing the solution of book (cc189).
+ */
 public class Solution04 {
+	public static int getNext(int n) {
+		int c = n;
+		int c0 = 0;
+		int c1 = 0;
+		while (((c & 1) == 0) && (c != 0)) {
+			c0++;
+			c >>= 1;
+		}
+		while ((c & 1) == 1) {
+			c1++;
+			c >>= 1;
+		}
+		if (c0 + c1 == 31 || c0 + c1 == 0)
+			return -1;
+		int p = c0 + c1; 
+		n |= (1 << p); 
+		n &= ~((1 << p) - 1);
+		n |= (1 << (c1 - 1)) - 1;
+		return n;
+	}
 
-}
+	public static int getPrev(int n) {
+		int temp = n;
+		int c0 = 0;
+		int c1 = 0;
+		while ((temp & 1) == 1) {
+			c1++;
+			temp >>= 1;
+		}
+		int p = c0 + c1;
+		n &= ((~0) << (p + 1)); 
+		int mask = (1 << (c1 + 1)) - 1;
+		n |= mask << (c0 - 1);
+		return n;
+	}
+
+	public static void main(String[] args) {
+		int n = 13948;
+		System.out.println(getNext(n));
+		System.out.println(getPrev(n));
+	}
